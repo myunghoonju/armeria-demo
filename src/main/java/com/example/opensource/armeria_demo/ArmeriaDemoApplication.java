56eeeb92ff2d8a95f99cc913f98cce2696af34c7
MyungHoon Ju <myunghoonju@gmail.com>
2024-12-18 20:34:06 +0900

    init commit

---
+++ src/main/java/com/example/opensource/armeria_demo/ArmeriaDemoApplication.java
@@ -0,0 +1,33 @@
+package com.example.opensource.armeria_demo;
+
+import com.linecorp.armeria.server.Server;
+import com.linecorp.armeria.server.ServerBuilder;
+import com.linecorp.armeria.server.docs.DocService;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+//@SpringBootApplication
+public class ArmeriaDemoApplication {
+
+	public static void main(String[] args) {
+//		SpringApplication.run(ArmeriaDemoApplication.class, args);
+		Server server = newServer(8080);
+		server.closeOnJvmShutdown();
+		server.start().join();
+		log.info("Armeria Demo Server started");
+	}
+
+	static Server newServer(int port) {
+		ServerBuilder sb = Server.builder();
+		DocService docService = DocService.builder()
+										  .exampleRequests(BlogService.class,
+														   "creatingPost",
+														   "{\"title\":\"My first blog\", \"content\":\"Hello Armeria!\"}")
+									      .build();
+
+		return sb.http(port)
+				 .annotatedService(new BlogService())
+//				 .service("/", (ctx, req) -> HttpResponse.of("Hello Armeria!") )
+				 .serviceUnder("/docs", docService).build();
+	}
+}
---
+++ src/main/java/com/example/opensource/armeria_demo/BlogPost.java
@@ -0,0 +1,37 @@
+package com.example.opensource.armeria_demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BlogPost {
+
+  private int id;
+
+  private String title;
+
+  private String content;
+
+  private long createdAt;
+
+  private long modifiedAt;
+
+  @JsonCreator
+  BlogPost(@JsonProperty("id") int id,
+           @JsonProperty("title") String title,
+           @JsonProperty("content") String content) {
+    this(id, title, content, System.currentTimeMillis());
+  }
+
+  BlogPost(int id, String title, String content, long createdAt) {
+    this(id, title, content, createdAt, createdAt);
+  }
+
+}
